c957c2d9de3471d67128c12406c6ee32ab4fde0b
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-08-16 14:52:03 +0300

    add minor note

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,14 +1,15 @@
 package handlers
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
 func SetupHandlers() {
 	http.HandleFunc("/", RootHandler)
+	// add handlers for each mapped path | app and forward returned JSON to caller
 }
 
-func RootHandler(writer http.ResponseWriter, request *http.Request)  {
+func RootHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(writer, "Root path of server, nothing to see here.")
 }
